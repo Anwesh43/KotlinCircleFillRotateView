b773505135c3a7b990b46d4b09f99e04c04ca081
Anwesh43 <anweshthecool0@gmail.com>
2018-02-26 02:02:44 +0530

    adding CircleFillRotateView

--- circlefillrotateview/src/main/java/ui/anwesome/com/circlefillrotateview/CircleFillRotateView.kt
+++ circlefillrotateview/src/main/java/ui/anwesome/com/circlefillrotateview/CircleFillRotateView.kt
@@ -6,3 +6,17 @@ package ui.anwesome.com.circlefillrotateview
 import android.content.*
 import android.graphics.*
 import android.view.*
+class CircleFillRotateView(ctx : Context): View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
