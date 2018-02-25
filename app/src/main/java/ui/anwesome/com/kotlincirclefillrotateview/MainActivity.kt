823bf37ce741ec3f6524339fb3d3fa99ecea7f58
Anwesh43 <anweshthecool0@gmail.com>
2018-02-26 02:29:50 +0530

    using CircleFillRotateView in activity

--- app/src/main/java/ui/anwesome/com/kotlincirclefillrotateview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincirclefillrotateview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlincirclefillrotateview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circlefillrotateview.CircleFillRotateView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleFillRotateView.create(this)
     }
 }
